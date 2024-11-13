7a01024be2d2b82c35042f58c06a70579f67db3f
mofowy <volodiachkax@gmail.com>
2024-11-13 02:16:41 +0200

    added images with captions

--- src/visualizer/bar_chart.py
+++ src/visualizer/bar_chart.py
@@ -10,6 +10,7 @@ class BarChart(Visualizer):
 
     def plot(self):
         plt.bar(self.data[self.x_col], self.data[self.y_col])
+        plt.title(f"Bar chart: {self.x_col} vs {self.y_col}")
         plt.xlabel(self.x_col)
         plt.ylabel(self.y_col)
         Exporter.export_plot(f'output/{self.x_col}_vs_{self.y_col}_bar_chart.png')
--- src/visualizer/scatter_plot.py
+++ src/visualizer/scatter_plot.py
@@ -10,6 +10,7 @@ class ScatterPlot(Visualizer):
 
     def plot(self):
         plt.scatter(self.data[self.x_col], self.data[self.y_col])
+        plt.title(f"Scatter plot: {self.x_col} vs {self.y_col}")
         plt.xlabel(self.x_col)
         plt.ylabel(self.y_col)
         Exporter.export_plot(f'output/{self.x_col}_vs_{self.y_col}_scatter_plot.png')
